19830302a5a6a426612e326f3c1d823f35650a2c
hong.kim <kimxuanhong@outlook.com>
2025-11-28 10:57:12 +0700

    enhance UI

---
+++ constants.js
@@ -0,0 +1,8 @@
+// Constants
+var COMPACT_WIDTH = 200;
+var COMPACT_HEIGHT = 40;
+var EXPANDED_WIDTH = 400;
+var EXPANDED_HEIGHT = 160;
+var ANIMATION_DURATION = 450; // Slower, smoother animation
+var BOX_MARGIN_TOP = 0;
+
